1dbe9a058bb6f9ef4e641672ecf0d8cac382a843
Joelle <joelle.saurer@devoteam.nl>
2021-07-08 09:37:12 +0200

    first master commit

--- switchStatement.r
+++ switchStatement.r
@@ -3,10 +3,10 @@ b <- "8"
  
 x <- switch(
     paste(a,b,sep=""),
-    "9"="Goot morning",
+    "9"="",
     "12"="Good Afternoon",
     "18"="Good Evening",
-    "21"="Good Night"
+    "21"=""
 )
  
 print(x)
